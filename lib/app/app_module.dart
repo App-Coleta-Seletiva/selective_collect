7e884049547fa3154572e7086bf43ec284f8746d
correiarangel <correiarangel@bol.com.br>
2022-04-16 23:34:17 -0300

    :construction:Fix test commit

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -5,7 +5,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppModule extends Module {
   @override
   final List<Bind> binds = [];
-
+//test
   @override
   final List<ModularRoute> routes = [
    // ModuleRoute(Modular.initialRoute, module: HomeModule()),
