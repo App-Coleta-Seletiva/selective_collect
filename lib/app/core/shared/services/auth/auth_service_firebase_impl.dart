0526d857869aea118654f14a6aa6b927eba9d44b
Victor Fonseca <victorlfonseca@gmail.com>
2022-05-12 19:49:45 -0300

    :sparkles: feat: firebase auth service impl.

--- lib/app/core/shared/services/auth/auth_service_firebase_impl.dart
+++ lib/app/core/shared/services/auth/auth_service_firebase_impl.dart
@@ -15,31 +15,103 @@ class FirebaseAuthService implements IAuthService {
       switch (e.code) {
         case 'invalid-email':
           throw AuthException(
-              message: 'Email inválido',
-              stackTrace: e.stackTrace ?? stackTrace);
-        // case 'invalid-email-verified':
-        //   throw AuthException(message: 'Email não verificado', stackTrace: stackTrace);
+            message: 'Email inválido',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'user-not-found':
+          throw AuthException(
+            message: 'Usuário não encontrado',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
 
         default:
+          throw AuthException(
+            message: 'Erro em recuperar a senha',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
       }
     }
   }
 
   @override
   UserEntityService? getCurrentUser() {
-    // TODO: implement getCurrentUser
-    throw UnimplementedError();
+    final user = _auth.currentUser;
+    if (user != null) {
+      return UserEntityService(user.email!);
+    } else {
+      return null;
+    }
   }
 
   @override
-  Future<void> loginWithEmail(LoginWithEmailParam param) {
-    // TODO: implement loginWithEmail
-    throw UnimplementedError();
+  Future<void> loginWithEmail(LoginWithEmailParam param) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: param.email, password: param.password);
+    } on FirebaseAuthException catch (e, stackTrace) {
+      switch (e.code) {
+        case 'invalid-email':
+          throw AuthException(
+            message: 'Email inválido',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'user-disabled':
+          throw AuthException(
+            message: 'Usuário desabilitado',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'user-not-found':
+          throw AuthException(
+            message: 'Usuário não encontrado',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'wrong-password':
+          throw AuthException(
+            message: 'Senha errada',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        default:
+          throw AuthException(
+            message: 'Erro ao tentar logar',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+      }
+    }
   }
 
   @override
-  Future<void> registerWithEmail(RegisterWithEmailParam param) {
-    // TODO: implement registerWithEmail
-    throw UnimplementedError();
+  Future<void> registerWithEmail(RegisterWithEmailParam param) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: param.email, password: param.password);
+    } on FirebaseAuthException catch (e, stackTrace) {
+      switch (e.code) {
+        case 'email-already-in-use':
+          throw AuthException(
+            message: 'Email já está em uso',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'invalid-email':
+          throw AuthException(
+            message: 'Email inválido',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'operation-not-allowed':
+          throw AuthException(
+            message: 'Conta desativada',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        case 'weak-password':
+          throw AuthException(
+            message: 'Senha fraca',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+        default:
+          throw AuthException(
+            message: 'Erro ao registrar',
+            stackTrace: e.stackTrace ?? stackTrace,
+          );
+      }
+    }
   }
 }
