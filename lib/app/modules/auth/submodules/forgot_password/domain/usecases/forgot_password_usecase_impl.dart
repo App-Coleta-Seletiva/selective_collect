07edbea6f1edf182d7f83a0854977f6009d44cbb
Lucas Amorim <58087933+lucassamorim@users.noreply.github.com>
2022-05-25 23:12:26 -0300

    :recycle: update: Ref. a impl. do usecase

--- lib/app/modules/auth/submodules/forgot_password/domain/usecases/forgot_password_usecase_impl.dart
+++ lib/app/modules/auth/submodules/forgot_password/domain/usecases/forgot_password_usecase_impl.dart
@@ -1,16 +1,25 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:selective_collect/app/modules/auth/submodules/forgot_password/domain/entities/forgot_password_entity.dart';
+
+import 'package:selective_collect/app/modules/auth/submodules/forgot_password/domain/repositories/i_forgot_password_reporitory.dart';
 
 import 'i_forgot_password_usecase.dart';
 
 class ForgotPasswordUsecaseImpl extends IForgotPasswordUsecase {
+  final IForgotPasswordRepository repository;
+  ForgotPasswordUsecaseImpl({
+    required this.repository,
+  });
+
   @override
-  Future<Either<Exception, bool>> call(
-      ForgotPasswordEntity forgotPassword) async {
-    var result = forgotPassword.email.contains("@");
-    if (result) {
-      return Right(result);
+  Future<Either<Exception, bool>> call(String email) async {
+    if (email.isEmpty) {
+      return Left(Exception("Empty email"));
     }
-    return Left(Exception("Invalid Email"));
+
+    if (!email.contains("@")) {
+      return Left(Exception("Invalid email"));
+    }
+
+    return await repository(email);
   }
 }
