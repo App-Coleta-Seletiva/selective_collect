0994e5d931e575bf73b2eeb6cc65f0d2d45806dd
Lucas Amorim <58087933+lucassamorim@users.noreply.github.com>
2022-05-25 23:13:09 -0300

    :recycle: update: Refratoramos o repository

--- lib/app/modules/auth/submodules/forgot_password/infra/repositories/forgot_password_reporitory.dart
+++ lib/app/modules/auth/submodules/forgot_password/infra/repositories/forgot_password_reporitory.dart
@@ -1,27 +1,22 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:selective_collect/app/modules/auth/submodules/forgot_password/domain/entities/forgot_password_entity.dart';
-import 'package:selective_collect/app/modules/auth/submodules/forgot_password/domain/usecases/i_forgot_password_usecase.dart';
 
 import '../../domain/repositories/i_forgot_password_reporitory.dart';
 import '../datasource/i_datasource.dart';
 
 class ForgotPasswordRepositoryImpl extends IForgotPasswordRepository {
-  final IForgotPasswordUsecase usecase;
   final IDatasource datasource;
 
   ForgotPasswordRepositoryImpl({
-    required this.usecase,
     required this.datasource,
   });
 
   @override
-  Future<Either<Exception, bool>> call(
-      ForgotPasswordEntity forgotPassword) async {
-    var result = await usecase(forgotPassword);
-    if (result is Exception) {
-      return result;
+  Future<Either<Exception, bool>> call(String email) async {
+    try {
+      var result = await datasource.recoveryPasswordByMail(email);
+      return right(result);
+    } catch (e) {
+      return left(Exception(e));
     }
-    //var email = result.fold
-    return await datasource.recoveryPasswordByMail("");
   }
 }
