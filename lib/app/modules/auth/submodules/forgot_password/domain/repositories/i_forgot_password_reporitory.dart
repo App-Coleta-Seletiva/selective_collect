23c82d171d082bfcc40bc3873c6e59d2964fc9df
Lucas Amorim <58087933+lucassamorim@users.noreply.github.com>
2022-05-25 23:07:39 -0300

    :recycle: update: Refratoramos o repository

--- lib/app/modules/auth/submodules/forgot_password/domain/repositories/i_forgot_password_reporitory.dart
+++ lib/app/modules/auth/submodules/forgot_password/domain/repositories/i_forgot_password_reporitory.dart
@@ -1,6 +1,5 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:selective_collect/app/modules/auth/submodules/forgot_password/domain/entities/forgot_password_entity.dart';
 
 abstract class IForgotPasswordRepository {
-  Future<Either<Exception, bool>> call(ForgotPasswordEntity forgotPassword);
+  Future<Either<Exception, bool>> call(String email);
 }
