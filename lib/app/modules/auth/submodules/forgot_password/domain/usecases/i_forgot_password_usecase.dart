2d1030ad6dc45717f8a6c382613c9998b1336362
Lucas Amorim <58087933+lucassamorim@users.noreply.github.com>
2022-05-25 23:11:59 -0300

    :recycle: update: Ref. a interface do usecase

--- lib/app/modules/auth/submodules/forgot_password/domain/usecases/i_forgot_password_usecase.dart
+++ lib/app/modules/auth/submodules/forgot_password/domain/usecases/i_forgot_password_usecase.dart
@@ -1,7 +1,5 @@
 import 'package:fpdart/fpdart.dart';
 
-import '../entities/forgot_password_entity.dart';
-
 abstract class IForgotPasswordUsecase {
-  Future<Either<Exception, bool>> call(ForgotPasswordEntity forgotPassword);
+  Future<Either<Exception, bool>> call(String email);
 }
