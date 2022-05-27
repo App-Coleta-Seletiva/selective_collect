0021959c849d5b51a375df229a6c917517486c90
Marcos Ribeiro <slproger@hotmail.com>
2022-05-27 12:15:12 -0300

    🐛 fix: error on initialization of Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    ModularApp(
-      module: AppModule(),
-      child: AppWidget(),
-    ),
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
