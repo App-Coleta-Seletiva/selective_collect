d7c9a006ea24c1c8d8ab93eb10f701879e430598
Luís Antonio <82737159+LA-98@users.noreply.github.com>
2022-05-27 00:54:02 -0300

    :white_check_mark: fix: init test repository

--- lib/app/modules/auth/submodules/forgot_password/login/domain/repositories/login_repository.dart
+++ lib/app/modules/auth/submodules/forgot_password/login/domain/repositories/login_repository.dart
@@ -1,4 +1,5 @@
 import 'package:fpdart/fpdart.dart';
+import '../../../../../../../core/shared/failures/exceptions.dart';
 import '../../infra/datasource/i_login_datasoure.dart';
 
 import '../../../../../../../core/shared/failures/i_app_exception.dart';
@@ -6,17 +7,38 @@ import 'i_login_repository.dart';
 import '../types/params.dart';
 
 class LoginRepository implements ILoginRepository {
-  final ILoginDatasouce _datasouce;
+  final ILoginDatasouce _datasource;
 
-  LoginRepository(this._datasouce);
+  LoginRepository(this._datasource);
 
   @override
-  Future<Either<IAppException, Unit>> call(LoginEmailParams params) async {
+  Future<Either<AuthException, Unit>> call(LoginEmailParams params) async {
+    // try {
+    //   await _datasouce(params);
+    //   return const Right(unit);
+    // } on AuthException catch (e) {
+    //   return Left(e);
+    // } catch (e, s) {
+    //   return Left(AuthException(message: e.toString(), stackTrace: s));
+    // }
+
     try {
-      await _datasouce(params);
-      return const Right(unit);
-    } on IAppException catch (e) {
-      return Left(e);
+      await _datasource(params);
+      return Right(unit);
+    } on AuthException catch (e, s) {
+      return Left(
+        AuthException(
+          message: e.toString(),
+          stackTrace: s,
+        ),
+      );
+    } catch (e) {
+      return Left(
+        AuthException(
+          message: e.toString(),
+          stackTrace: StackTrace.empty,
+        ),
+      );
     }
   }
 }
--- test/app/mock/mock.dart
+++ test/app/mock/mock.dart
@@ -1,8 +1,10 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:selective_collect/app/core/shared/failures/i_app_exception.dart';
 import 'package:selective_collect/app/modules/auth/submodules/forgot_password/login/domain/entities/login_entity.dart';
 import 'package:selective_collect/app/modules/auth/submodules/forgot_password/login/domain/repositories/i_login_repository.dart';
 import 'package:selective_collect/app/modules/auth/submodules/forgot_password/login/domain/types/params.dart';
+import 'package:selective_collect/app/modules/auth/submodules/forgot_password/login/infra/datasource/i_login_datasoure.dart';
 
 class RepositoryMock extends Mock implements ILoginRepository {}
 
@@ -11,3 +13,7 @@ class LoginEmailParamsMock extends Mock implements LoginEmailParams {}
 class LoginEntityMock extends Mock implements LoginEntity {}
 
 class UnitMock extends Mock implements Unit {}
+
+class LoginDatasourseMock extends Mock implements ILoginDatasouce {}
+
+class AppExceptionMock extends Mock implements IAppException {}
