0caee0f692b2cebbb6ef28532d9dd6b0a4d10858
Marcos Ribeiro <slproger@hotmail.com>
2022-05-27 16:25:05 -0300

    🚚 rename (logout) : Com "I" file name uppercase

--- lib/app/modules/auth/submodules/login/domain/usecases/I_logout_usecase.dart
+++ lib/app/modules/auth/submodules/login/domain/usecases/I_logout_usecase.dart
@@ -1,6 +1,7 @@
 import 'package:fpdart/fpdart.dart';
+
 import '../../../../../../core/shared/failures/i_app_exception.dart';
 
 abstract class ILogoutUsecase {
-  Future<Either< IAppException, void >> call();
+  Future<Either<IAppException, void>> call();
 }
