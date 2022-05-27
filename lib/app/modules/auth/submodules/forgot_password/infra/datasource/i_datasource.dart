5b7383b3ef11873dc08160667ff3387ac0aab93d
Marcos Ribeiro <slproger@hotmail.com>
2022-05-27 12:05:35 -0300

    🔥 remove: feat not used

--- lib/app/modules/auth/submodules/forgot_password/infra/datasource/i_datasource.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-
-import '../../../../../../core/shared/failures/forgot_password_failure.dart';
-
-abstract class IDatasource {
-  Future<Either<Failure, bool>> recoveryPasswordByMail(String email);
-}
