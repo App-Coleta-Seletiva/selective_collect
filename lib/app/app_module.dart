4811b4b2c208564bed48aa3a78cd690a8223ba46
Marcos Ribeiro <slproger@hotmail.com>
2022-05-30 18:13:03 -0300

    🚚 hasura_service: moved to shared folder

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'core/service/hasura_service.dart';
 import 'core/shared/services/auth/auth_service_firebase_impl.dart';
+import 'core/shared/services/hasura_service/hasura_service.dart';
 import 'modules/auth/auth_module.dart';
 import 'modules/auth/guards/auth_guard.dart';
 import 'modules/home/home_module.dart';
