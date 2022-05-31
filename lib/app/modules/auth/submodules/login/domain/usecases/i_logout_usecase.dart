3435943a3c43240205a04bb87b2b12f97095f36c
Marcos Ribeiro <slproger@hotmail.com>
2022-05-31 08:16:11 -0300

    :adhesive_bandage: fix: patching branch

--- lib/app/modules/auth/submodules/login/domain/usecases/i_logout_usecase.dart
+++ lib/app/modules/auth/submodules/login/domain/usecases/i_logout_usecase.dart
@@ -1,6 +1,7 @@
+
 import '../../../../../../core/shared/failures/i_app_exception.dart';
 import '../../../../../../core/types/either.dart';
 
 abstract class ILogoutUsecase {
-  Future<Either<IAppException, Unit>> call();
+  Future<Either< IAppException, void >> call();
 }
