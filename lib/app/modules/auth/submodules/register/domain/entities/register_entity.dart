40a24199d9720643336c6ed0e8daaa795123c8f4
jozadaque <jozadaquesousa@gmail.com>
2022-06-01 21:53:28 -0300

    ✨feat: create RegisterEntity

--- lib/app/modules/auth/submodules/register/domain/entities/register_entity.dart
+++ lib/app/modules/auth/submodules/register/domain/entities/register_entity.dart
@@ -1,14 +1,19 @@
 class RegisterEntity {
-  final int? id;  //* um novo usuário não possui id antes de ser salvo na base de dados?
+  final int?
+      id; //* um novo usuário não possui id antes de ser salvo na base de dados?
   final String email;
   final String username;
-  final String? uuid;  //* um novo usuário não possui uuid antes de ser cadastrado na api de login?
-
+  final String
+      uuid; //* um novo usuário não possui uuid antes de ser cadastrado na api de login?
 
   RegisterEntity({
     this.id,
     required this.email,
     required this.username,
-    this.uuid,
+    required this.uuid,
   });
 }
+
+
+// dados  -> firebase
+// modulo -> hasura
