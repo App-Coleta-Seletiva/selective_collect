c3bb2f53c5a0af3f998140a933e6f11039268c7f
jozadaque <jozadaquesousa@gmail.com>
2022-06-03 23:35:29 -0300

    ✨feat:replaced RegisterParams params by RegisterWithEmailParam params

--- lib/app/modules/auth/submodules/register/domain/usecases/register_usecase.dart
+++ lib/app/modules/auth/submodules/register/domain/usecases/register_usecase.dart
@@ -1,10 +1,8 @@
-import 'package:selective_collect/app/core/shared/failures/register_errors.dart';
-
 import '../../../../../../core/shared/failures/i_app_exception.dart';
-
+import '../../../../../../core/shared/failures/register_errors.dart';
+import '../../../../../../core/shared/services/auth/i_auth_service.dart';
 import '../../../../../../core/types/either.dart';
 import '../repositories/i_register_repository.dart';
-import '../types/register_params.dart';
 import 'i_register_usecase.dart';
 
 class RegisterUsecaseImpl implements IRegisterUsecase {
@@ -13,7 +11,8 @@ class RegisterUsecaseImpl implements IRegisterUsecase {
   RegisterUsecaseImpl(this.repository);
 
   @override
-  Future<Either<IAppException, Unit>> call(RegisterParams params) async {
+  Future<Either<IAppException, Unit>> call(
+      RegisterWithEmailParam params) async {
     if (params.email.isEmpty) {
       return left(const EmailError());
     }
