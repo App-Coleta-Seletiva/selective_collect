d7292fca0896ac6d115591d225d4f02de159dea3
jozadaque <jozadaquesousa@gmail.com>
2022-06-15 22:03:29 -0300

    ✨feat: create class to catch register errors

--- lib/app/core/shared/failures/register_errors.dart
+++ lib/app/core/shared/failures/register_errors.dart
@@ -1,15 +1,31 @@
-import 'errors.dart';
+import 'package:selective_collect/app/core/shared/failures/i_app_exception.dart';
 
-class EmailError extends AppError {
-  const EmailError({
+abstract class IErrosRegister extends IAppException {
+  IErrosRegister({required String message, required StackTrace? stackTrace})
+      : super(message: message, stackTrace: stackTrace);
+}
+
+class EmailError extends IErrosRegister {
+  EmailError({
     String message = 'E-mail invalido',
     StackTrace? stackTrace,
   }) : super(message: message, stackTrace: stackTrace);
 }
 
-class PasswordError extends AppError {
-  const PasswordError({
+class PasswordError extends IErrosRegister {
+  PasswordError({
     String message = 'Senha deve possuir 8 ou mais caracteres',
     StackTrace? stackTrace,
   }) : super(message: message, stackTrace: stackTrace);
+
 }
+
+class RegisterError extends IErrosRegister {
+  RegisterError({
+    String message = 'Erro ao registrar usuário.',
+    StackTrace? stackTrace,
+  }) : super(message: message, stackTrace: stackTrace);
+
+}
+
+
